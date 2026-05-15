9a9932355ded92d038530833efad7286f9eabf55
Joseph Mearman <joseph@mearman.co.uk>
2026-05-15 11:02:42 +0100

    fix: enable autodocs for all stories and fix blank docs page

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -2,13 +2,17 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
     stories: ["../stories/**/*.stories.tsx"],
-    addons: ["@storybook/addon-vitest", "@storybook/addon-a11y"],
+    addons: [
+        "@storybook/addon-vitest",
+        "@storybook/addon-a11y",
+        "@storybook/addon-docs",
+    ],
     framework: {
         name: "@storybook/react-vite",
         options: {},
     },
     docs: {
-        autodocs: true,
+        autodocs: "tag",
     },
     staticDirs: [{ from: "../../core/dist", to: "/" }],
 };
