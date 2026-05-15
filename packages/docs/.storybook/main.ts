dbab5ce2d3ccacd26793df125ae3d62dc301c00c
Joseph Mearman <joseph@mearman.co.uk>
2026-05-15 09:39:43 +0100

    docs: add README landing page and autodocs to Storybook

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -1,8 +1,22 @@
+import remarkGfm from "remark-gfm";
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-    stories: ["../stories/**/*.stories.tsx"],
-    addons: ["@storybook/addon-vitest", "@storybook/addon-a11y"],
+    stories: ["../stories/**/*.mdx", "../stories/**/*.stories.tsx"],
+    addons: [
+        "@storybook/addon-vitest",
+        "@storybook/addon-a11y",
+        {
+            name: "@storybook/addon-docs",
+            options: {
+                mdxPluginOptions: {
+                    mdxCompileOptions: {
+                        remarkPlugins: [remarkGfm],
+                    },
+                },
+            },
+        },
+    ],
     framework: {
         name: "@storybook/react-vite",
         options: {},
