45434fd3be948c0840a7d0328411e7f24585d63c
Joseph Mearman <joseph@mearman.co.uk>
2026-05-16 22:51:35 +0100

    chore(docs): add @storybook/addon-links
    Register the @storybook/addon-links addon in Storybook's main config so
    stories can cross-link to related stories via the linkTo() helper.

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -7,6 +7,7 @@ const config: StorybookConfig = {
         "@storybook/addon-a11y",
         "@storybook/addon-docs",
         "@storybook/addon-themes",
+        "@storybook/addon-links",
     ],
     framework: {
         name: "@storybook/react-vite",
